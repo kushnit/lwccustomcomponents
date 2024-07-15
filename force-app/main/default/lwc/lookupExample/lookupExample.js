267e04f3f1726dbbeb169968e60552a0451646b0
Nitin Kushawaha <kushnit@gmail.com>
2024-07-14 23:14:53 -0400

    lookup example

---
+++ force-app/main/default/lwc/lookupExample/lookupExample.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class LookupExample extends LightningElement {
+    handleSuccess(event) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: event.detail.apiName + ' created.',
+                variant: 'success',
+            })
+        );
+    }
+
+    handleChange(event) {
+        console.log('You selected an account: ' + event.detail.value[0]);
+    }
+}
